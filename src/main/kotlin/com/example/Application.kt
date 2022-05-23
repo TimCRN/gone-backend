a7b2d7a5cfd4ba1a69540a07d17d0cf58f915e52
TimCRN <tim.crounse@gmail.com>
2022-05-23 18:04:40 +0200

    Aplication.kt edits

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,25 +1,31 @@
 package com.example
 
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import com.example.plugins.*
 import com.example.repository.DatabaseFactory
+import com.example.repository.UserRepository
+import com.example.services.JwtService
+import com.example.services.hash
+import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.application.*
-
-//fun main() {
-//    embeddedServer(Netty, port = 8080, watchPaths = listOf("gone-backend"), host = "0.0.0.0") {
-//        DatabaseFactory.init()
-//        configureSecurity()
-//        configureRouting()
-//    }.start(wait = true)
-//}
+import io.ktor.server.plugins.cors.routing.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+
     DatabaseFactory.init()
-    configureSecurity()
-    configureRouting()
+    val userRepository = UserRepository()
+    val jwtService = JwtService()
+    val hashFunction = { s:String -> hash(s) }
+
+    if(dotenv()["DEV_MODE"].toBoolean()){
+        install(CORS)
+    }
+
+    configureContentNegotiation()
+    configureSecurity(userRepository,jwtService)
+    configureBasicRouting()
+    configureAuthRouting(userRepository,jwtService,hashFunction)
 }
