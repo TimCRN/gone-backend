d1b67107280dd62d8d7d21bd8280a85e3533edc3
TimCRN <tim.crounse@gmail.com>
2022-05-23 18:01:34 +0200

    Hashing & JWT service added

---
+++ src/main/kotlin/com/example/services/JwtService.kt
@@ -0,0 +1,38 @@
+package com.example.services
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.example.data.model.User
+import io.github.cdimascio.dotenv.dotenv
+import io.ktor.util.*
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+private val hashKey = dotenv()["HASH_SECRET"].toByteArray()
+private val hmacKey = SecretKeySpec(hashKey,"HmacSHA1")
+
+fun hash(password: String): String {
+    val hmac = Mac.getInstance("HmacSHA1")
+    hmac.init(hmacKey)
+    return hex(hmac.doFinal(password.toByteArray(Charsets.UTF_8)))
+}
+
+class JwtService {
+    private val issuer = "goneBackend"
+    private val jwtSecret = dotenv()["JWT_SECRET"]
+    private val algorithm = Algorithm.HMAC512(jwtSecret)
+
+    val verifier:JWTVerifier = JWT
+        .require(algorithm)
+        .withIssuer(issuer)
+        .build()
+
+    fun generateToken(user: User): String {
+        return JWT.create()
+            .withSubject("GoneAuthentication")
+            .withIssuer(issuer)
+            .withClaim("email",user.email)
+            .sign(algorithm)
+    }
+}
